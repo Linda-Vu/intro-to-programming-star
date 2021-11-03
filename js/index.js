72b5a69c52b571cf568697427865649b3fba6e7e
Linda Vu <tienlindavu@gmail.com>
2021-11-02 22:54:37 -0400

    feat: add contact form

--- js/index.js
+++ js/index.js
@@ -20,3 +20,40 @@ for (let i = 0; i < skills.length; i++) {
   skill.innerText = skills[i]
   skillsList.appendChild(skill)
 }
+
+const messageForm = document.querySelector('form[name=leave_message]')
+
+messageForm.addEventListener('submit', function (event) {
+  event.preventDefault()
+
+  const nameEl = event.target.name.value
+  const emailEl = event.target.email.value
+  const textAreaEl = event.target.message.value
+
+  console.log(nameEl, emailEl, textAreaEl)
+
+  const messageSection = document.getElementById('messages')
+
+
+  const messageList = messageSection.querySelector('ul')
+
+  const newMessage = document.createElement('li')
+
+  newMessage.innerHTML = `<span><a href="mailto:${emailEl}">${nameEl} </a><a>${textAreaEl} </a></span>`
+
+  const removeButton = document.createElement('button')
+
+  removeButton.innerText = 'remove'
+
+  removeButton.addEventListener('click', function (event) {
+    const entry = removeButton.parentNode
+    entry.remove()
+  })
+
+  newMessage.appendChild(removeButton)
+
+  messageList.appendChild(newMessage)
+
+  messageForm.reset();
+  
+})
